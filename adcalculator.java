e4b534cbce8db17fe5263bfdd8b2a5fccbf66785
sosina19 <yeshidnbersosina@gmail.com>
2026-01-09 08:13:18 +0300

    add new methods to the adcalculator .java system

--- adcalculator.java
+++ adcalculator.java
@@ -18,8 +18,11 @@ public class adcalculator {
             System.out.println("     =>press 6 to power(Exponent)");
             System.out.println("     =>press 7 to Modulus operation");
             System.out.println("     =>press 8 to Percentage operation");
-            System.out.println("     =>press 9 to view developers");
-            System.out.println("     =>press 10 to exit");
+            System.out.println("     =>press 9 to factorial operation");
+            System.out.println("     =>press 10 to average operation");
+            System.out.println("     =>press 11 to absolute value operation");
+            System.out.println("     =>press 12 to view developers");
+            System.out.println("     =>press 13 to exit");
             System.out.print("         =>please enter ur chooise:".toUpperCase());
             ch = input.nextInt();
             System.out.println("");
@@ -56,8 +59,20 @@ public class adcalculator {
                     percentage();
                     addmoreper();
                 }
-                 case 9 -> developers();
-                case 10-> {
+                  case 9 -> {
+                    factorial();
+                    addmoref();
+                }
+                  case 10 -> {
+                   absolute();
+                    addmoreabs();
+                }
+                  case 11 -> {
+                    average();
+                    addmoreavg();
+                }
+                 case 12 -> developers();
+                case 13-> {
                 }
                 default -> System.out.println(" \"error\"please enter the correct chooise".toUpperCase());
             }
@@ -240,6 +255,84 @@ public static void percentage() {
     System.out.println("RESULT = " + (total * percent / 100));
 }
 
+public static void factorial() {
+    Scanner in = new Scanner(System.in);
+
+    try {
+        System.out.print("ENTER A NUMBER: ");
+        int num = in.nextInt();
+
+        if (num < 0) {
+            System.out.println("FACTORIAL NOT DEFINED FOR NEGATIVE NUMBERS.");
+            return;
+        }
+
+        long fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("FACTORIAL = " + fact);
+    } catch (InputMismatchException e) {
+        System.out.println("INVALID INPUT! ENTER AN INTEGER.");
+        in.next();
+    }
+}
+public static void absolute() {
+    Scanner in = new Scanner(System.in);
+
+    try {
+        System.out.print("ENTER A NUMBER: ");
+        double num = in.nextDouble();
+
+        System.out.println("ABSOLUTE VALUE = " + Math.abs(num));
+    } catch (InputMismatchException e) {
+        System.out.println("INVALID INPUT! ENTER A NUMBER.");
+        in.next();
+    }
+}
+public static void average() {
+    Scanner in = new Scanner(System.in);
+
+    int n;
+    try {
+        System.out.print("HOW MANY NUMBERS DO YOU WANT TO AVERAGE? ");
+        n = in.nextInt();
+
+        if (n <= 0) {
+            System.out.println("NUMBER COUNT MUST BE GREATER THAN 0.");
+            return;
+        }
+
+        if (n > 100) {
+            System.out.println("TOO MANY NUMBERS! MAX IS 100.");
+            return;
+        }
+    } catch (InputMismatchException e) {
+        System.out.println("INVALID INPUT! ENTER AN INTEGER.");
+        in.next();
+        return;
+    }
+
+    double sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        try {
+            System.out.print("ENTER NUMBER " + i + ": ");
+            sum += in.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("INVALID NUMBER! TRY AGAIN.");
+            in.next();
+            i--; // retry same index
+        }
+    }
+
+    double avg = sum / n;
+    System.out.println("AVERAGE = " + avg);
+}
+
+
+
 
     public static void addmore() {
         Scanner input = new Scanner(System.in);
@@ -388,6 +481,61 @@ public static void addmoreper() {
                 break;
         }
     } while (!inp.equals("exit"));
+}
+public static void addmoref() {
+    Scanner input = new Scanner(System.in);
+    String inp = "";
+
+    do {
+        System.out.println("IF U WANT FACTORIAL AGAIN PRESS \"Y\" ELSE WRITE \"ANYTHING\"".toUpperCase());
+        inp = input.next().toLowerCase();
+
+        switch (inp) {
+            case "y":
+                factorial();
+                break;
+            default:
+                System.out.println("REDIRECTING TO HOME PAGE".toUpperCase());
+                adcalculator.main(null);
+                break;
+        }
+    } while (!inp.equals("exit"));
+}
+public static void addmoreabs() {
+    Scanner input = new Scanner(System.in);
+    String inp = "";
+
+    do {
+        System.out.println("IF U WANT ABSOLUTE AGAIN PRESS \"Y\" ELSE WRITE \"ANYTHING\"".toUpperCase());
+        inp = input.next().toLowerCase();
+
+        switch (inp) {
+            case "y":
+                absolute();
+                break;
+            default:
+                System.out.println("REDIRECTING TO HOME PAGE".toUpperCase());
+                adcalculator.main(null);
+                break;
+        }
+    } while (!inp.equals("exit"));
+}
+public static void addmoreavg() {
+    Scanner input = new Scanner(System.in);
+    String inp;
+
+    do {
+        System.out.println("IF YOU WANT TO AVERAGE MORE PRESS \"y\" ELSE TYPE \"exit\"");
+        inp = input.next().toLowerCase();
+
+        if (inp.equals("y")) {
+            average();
+        } else {
+            System.out.println("REDIRECTING TO HOME PAGE");
+            adcalculator.main(null);
+            return;
+        }
+    } while (true);
 }
 
   
